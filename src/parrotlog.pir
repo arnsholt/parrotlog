112383d1674d3edd1691f138b7a295cd19232a9f
Arne <arnsholt@gmail.com>
2010-03-28 04:08:02 +0200

    Initial choose/fail implementation. Started work on grammar.

--- src/parrotlog.pir
+++ src/parrotlog.pir
@@ -12,6 +12,54 @@
     parrotns.'export_to'(hllns, imports)
 .end
 
+# Non-deterministic search. Choose an element from a list of options, with the
+# option to backtrack if it turns out it was an invalid value.
+.sub 'choose'
+    .param pmc options :slurpy
+    .local pmc cc
+    .local pmc chosen
+    .local pmc paths
+
+    # If we have no options to choose from a previous search has failed.
+    if options goto got_options
+    'fail'()
+  got_options:
+    chosen = shift options
+
+    # Create a continuation for our current state and save it on the stack of
+    # active choice points.
+    cc = new 'Continuation'
+    set_addr cc, recurse
+    paths = get_global '!paths'
+    push paths, cc
+
+    say chosen
+    .return (chosen)
+
+  recurse:
+    .tailcall 'choose'(options :flat)
+.end
+
+# Signal that choose() has picked an invalid element, and invoke the saved
+# continuation.
+.sub 'fail'
+    .local pmc cc
+    .local pmc paths
+
+    paths = get_global '!paths'
+
+    if paths goto got_paths
+    cc = get_global '!topcc'
+    goto call_cc
+  got_paths:
+    cc = shift paths
+
+  call_cc:
+    cc()
+.end
+
+# TODO: Implement mark() and cut().
+
 .include 'src/gen/parrotlog-grammar.pir'
 .include 'src/gen/parrotlog-actions.pir'
 .include 'src/gen/parrotlog-compiler.pir'
--- src/parrotlog/Grammar.pm
+++ src/parrotlog/Grammar.pm
@@ -6,18 +6,114 @@ This is the grammar for parrotlog in Perl 6 rules.
 
 grammar parrotlog::Grammar is HLL::Grammar;
 
+# XXX: nqp-rx doesn't like grammar items with -, so we use _ instead.
 token TOP {
-    <statementlist>
+    <prolog_text>*
     [ $ || <.panic: "Syntax error"> ]
 }
 
-## Lexer items
+proto token prolog_text { <...> }
+rule prolog_text:type<directive> { <term> '.' }
+rule prolog_text:type<clause> { }
 
-# This <ws> rule treats # as "comment to eol".
-token ws {
-    <!ww>
-    [ '#' \N* \n? | \s+ ]*
+proto token term { <...> }
+
+rule atom:sym<[ ]> { <sym> }
+rule atom:sym<{ }> { <sym> }
+
+# Tokens: section 6.4
+
+# Layout text, section 6.4.1.
+token ws { <layout_text>+ } # Layout text separates stuff, so we set <ws> to that
+token layout_text { [ <layout_char> | <comment> ]+ }
+proto token comment { <...> }
+token comment:sym<line> { <line_comment> \N \n }
+token comment:sym<bracketed> {
+    '/*'
+    [ <-[/]> | <!after '*' > '/' ] *
+    '*/'
 }
+
+# Name tokens: section 6.4.2
+proto token name_token { <...> }
+token name_token:sym<identifier> { <small_char> <alnum_char>* }
+token name_token:sym<graphic> { <+graphic_char +backslash>+ }
+# TODO: This stuff should probably be implemented with quote_EXPR. I just have
+# to figure out how quote_EXPR works...
+#token name_token:sym<quoted> {
+#    <single_quote_char> [ <single_quoted_char> | \\ \n ]* <single_quote_char>
+#}
+#token semicolon_token { <semicolon_char> }
+#token cut_token { <cut_char> }
+#proto token single_quoted_char { <...> }
+#token single_quoted_char:sym<non-quote> { <+graphic_char
+#                                           +alnum_char
+#                                           +solo_char
+#                                           +space> }
+#token single_quoted_char:sym<meta-escape> { <backslash_char> <meta_char> }
+#token single_quoted_char:sym<control-escape> { 
+
+# Variables: section 6.4.3
+proto token variable { <...> }
+token variable:sym<anonymous> { <underscore> }
+token variable:sym<named> { <underscore> <alnum-char>+ | <capital-char> <alnum-char>* }
+
+# Characters: section 6.5
+token char { <+graphic_char +alnum_char +solo_char +layout_char +meta_char> }
+
+# Graphic characters: section 6.5.1
+token graphic_char { <[#$&*+_./:<=>?@^~]> }
+
+# Alphanumerics: section 6.5.2
+token alnum_char { <+alpha +digit> }
+token alpha_char { <+underscore letter> }
+token letter_char { <+capital +small> }
+token small_char { <lower> }
+token capital_char { <upper> }
+token decdigit_char { <[0..9]> }
+token bindigit_char { <[01]> }
+token octdigit_char { <[0..7]> }
+token hexdigit_char { <xdigit> }
+token underscore_char { <[_]> }
+
+# Solo characters: section 6.5.3
+token solo_char { <+cut_char
+                   +open_char
+                   +close_char
+                   +comma_char
+                   +semicolon_char
+                   +open_list_char
+                   +close_list_char
+                   +open_curly_char
+                   +close_curly_char
+                   +head_tail_char
+                   +line_comment_char> }
+token cut_char { <[!]> }
+token open_char { <[(]> }
+token close_char { <[)]> }
+token comma_char { <[,]> }
+token semicolon_char { <[;]> }
+token open_list_char { <[\[]> }
+token close_list_char { <[\]]> }
+token open_curly_char { <[{]> }
+token close_curly_char { <[}]> }
+token head_tail_char { <[|]> }
+token line_comment_char { <[%]> }
+
+# Layout characters: section 6.5.4
+token layout_char { <space> | \n }
+
+# Meta characters: section 6.5.5
+token meta_char { <+backslash_char
+                   +single_quote_char
+                   +double_quote_char
+                   +back_quote_char> }
+token backslash_char { <[\\]> }
+token single_quote_char { <[']> }
+token double_quote_char { <["]> }
+token back_quote_char { <[`]> }
+
+## Lexer items
 
 ## Statements
 
@@ -38,20 +134,8 @@ token term:sym<integer> { <integer> }
 token term:sym<quote> { <quote> }
 
 proto token quote { <...> }
-token quote:sym<'> { <?[']> <quote_EXPR: ':q'> }
+token quote:sym<'> { <?[']> <quote_EXPR: ':q'> } # For vi: '
 token quote:sym<"> { <?["]> <quote_EXPR: ':qq'> }
 
-## Operators
-
-INIT {
-    parrotlog::Grammar.O(':prec<u>, :assoc<left>',  '%multiplicative');
-    parrotlog::Grammar.O(':prec<t>, :assoc<left>',  '%additive');
-}
-
-token circumfix:sym<( )> { '(' <.ws> <EXPR> ')' }
-
-token infix:sym<*>  { <sym> <O('%multiplicative, :pirop<mul>')> }
-token infix:sym</>  { <sym> <O('%multiplicative, :pirop<div>')> }
-
-token infix:sym<+>  { <sym> <O('%additive, :pirop<add>')> }
-token infix:sym<->  { <sym> <O('%additive, :pirop<sub>')> }
+# TODO: Operators. ISO Prolog allows for user defined operators. That should
+# probably be implemented using nqp's extendable grammar features.
