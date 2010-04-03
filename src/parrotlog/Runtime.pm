8eaa885081f0c22bf4b1e0349af52ebf3f5cfa66
Arne Skjærholt <arnsholt@gmail.com>
2010-04-03 19:31:04 +0200

    Unification; hijacked interpreter to test internals.

--- src/parrotlog/Runtime.pm
+++ src/parrotlog/Runtime.pm
@@ -83,9 +83,18 @@ class Term {
     has $!arity;
     has @!args;
 
+    method from_data($functor, *@args) {
+        my $term := Term.new;
+        $term.functor($functor);
+        $term.arity(@args);
+        $term.args(|@args);
+
+        return $term;
+    }
+
     # XXX: No objects have a defined() method.
     method functor($ctor?) {
-        if $ctor.defined {
+        if $ctor {
             $!functor := $ctor;
         }
 
@@ -93,7 +102,7 @@ class Term {
     }
 
     method arity($arity?) {
-        if $arity.defined {
+        if $arity {
             $!arity := $arity;
         }
 
@@ -101,7 +110,7 @@ class Term {
     }
 
     method args(*@args) {
-        if @args.defined {
+        if @args {
             @!args := @args;
         }
 
@@ -110,26 +119,43 @@ class Term {
 }
 
 sub unify($paths, $x, $y) {
+    if $x =:= $y {
+        # XXX: ATM NQP ignores bare returns, so we return a value.
+        return 1;
+    }
+
     if $x ~~ Term && $y ~~ Term {
+        # First, check that the functors and arities are the same.
         if $x.functor ne $y.functor
         || $x.arity   != $y.arity {
             fail($paths);
         }
 
-        # TODO: Make sure argument lists unify.
+        # Then, make sure the argument lists unify as well.
+        my $i := 0;
+        my @x := $x.args;
+        my @y := $y.args;
+        while $i < $x.args {
+            unify($paths, @x[$i], @y[$i]);
+            $i++;
+        }
+
+        return 1;
     }
     elsif $x ~~ Var && $y ~~ Term
     ||    $x ~~ Term && $y ~~ Var {
-        # simplify control flow.
+        # To simplify control flow.
         if $x ~~ Term {
             my $tmp := $x;
             $x := $y;
             $y := $tmp;
         }
 
+        # TODO
         $x.bind($y);
     }
     else {
+        # TODO
     }
 =begin sketch
 
@@ -146,6 +172,10 @@ our $tests;
 
 sub say($str) {
     pir::say($str);
+}
+
+sub diag($str) {
+    say("# $str");
 }
 
 sub ok($msg?) {
@@ -166,6 +196,37 @@ sub not_ok($msg?) {
     else {
         say("not ok $tests");
     }
+}
+
+sub succeeds(&block, $msg?) {
+    my $*paths := paths();
+    if $*paths {
+        &block();
+        ok($msg);
+    }
+    else {
+        not_ok($msg);
+    }
+}
+
+sub fails(&block, $msg?) {
+    my $*paths := paths();
+    if $*paths {
+        &block();
+        not_ok($msg);
+    }
+    else {
+        ok($msg);
+    }
+}
+
+sub unifies($x, $y, $msg?) {
+    succeeds( { unify($*paths, $x, $y); }, $msg);
+}
+
+# XXX: Needs a better name...
+sub not_unifies($x, $y, $msg?) {
+    fails( { unify($*paths, $x, $y); }, $msg);
 }
 
 sub plan($count?) {
@@ -189,6 +250,29 @@ sub MAIN() {
     else {
         ok("backtracking");
     }
+
+    # Test unification of terms.
+    my $atom := Term.from_data("atom");
+    ok("creating atoms");
+    unifies($atom, $atom, "unification of atom to itself");
+
+    my $other_atom := Term.from_data("atom");
+    unifies($atom, $other_atom, "unification of atom to identical atom");
+
+    my $different := Term.from_data("different");
+    not_unifies($atom, $different, "non-unification of unequal atoms");
+
+    my $complex := Term.from_data("term", $atom);
+    unifies($complex, $complex, "unification of complex term to itself");
+
+    my $other_complex := Term.from_data("term", $other_atom);
+    unifies($complex, $other_complex, "unification of complex term to identical complex term");
+
+    my $different_complex := Term.from_data("different", $atom);
+    not_unifies($complex, $different_complex, "non-unification of different complex terms");
+
+    # Test unification of variables.
+    # TODO
 
     plan();
 }
