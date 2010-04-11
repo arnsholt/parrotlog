72a85607618f53088c4cf1167bfb01e2f4c23971
Arne Skjærholt <arnsholt@gmail.com>
2010-04-11 03:29:28 +0200

    Added more token types, tweaked some indentation; killed default gramamr.

--- src/parrotlog/Grammar.pm
+++ src/parrotlog/Grammar.pm
@@ -45,13 +45,14 @@ token prolog_text { [ <directive> | <clause> ]* }
 
 # Directives: section 6.2.1.1
 token directive { <directive_term> <.end> }
+# TODO: The principal functor of the term is ':-'
 # TODO: The term in directive term has priority 1201.
 token directive_term { <term> }
 
 # Clauses: section 6.2.1.2
 token clause { <clause_term> <.end> }
 # TODO: The term in clause term has priority 1201.
-# TODO: The principal functor of term cannot be :-
+# TODO: The principal functor of the term is not ':-'
 token clause_term { <term> }
 
 # Data: section 6.2.2
@@ -72,6 +73,7 @@ token term:sym<float> { '-'? <float_number> }
 # Atoms: section 6.3.1.3
 # TODO: Priority is 0.
 token term:sym<atom> { <!operator> <atom> }
+
 
 proto token atom { <...> }
 token atom:sym<name> { <name> }
@@ -133,15 +135,53 @@ token prefix:sym<\\>          { <sym> <O('fy')> }
 token infix:sym<@>            { <sym> <O('xfx')> }
 token infix:sym<:>            { <sym> <O('xfx')> }
 
+# Compound terms - list notation: section 6.3.5
+token term:<list> { <.open_list> <items> <.close_list> }
+token items { $<head>=[<.EXPR> ** <.comma>] [<.ht_sep> $<tail>=<.EXPR>]? }
+
+# Compound terms - curly bracket notation: section 6.3.6
+token term:<curly> { <.open_curly> <term> <.close_curly> }
+
+# Compound terms - character code list notation: section 6.3.7
+# TODO
+
 # Tokens: section 6.4
+proto token token { <...> }
+token token:sym<name> { <name_token> }
+token token:sym<variable> { <variable_token> }
+token token:sym<integer> { <integer_token> }
+token token:sym<float> { <float_token> }
+# TODO: Char code list.
+token token:sym<open> { <open_token> }
+token token:sym<close> { <close_token> }
+token token:sym<open_list> { <open_list_token> }
+token token:sym<close_list> { <close_list_token> }
+token token:sym<open_curly> { <open_curly_token> }
+token token:sym<close_curly> { <close_curly_token> }
+token token:sym<head_tail_separator> { <head_tail_separator_token> }
+token token:sym<comma> { <comma_token> }
+token token:sym<end> { <end_token> }
+
 token name { <.ws> <name_token> }
 token variable { <.ws> <variable_token> }
+token integer { <.ws> <integer_token> }
+token float { <.ws> <float_token> }
+# TODO: Char code list.
+token open_ct { <.ws> <open_token> }
+token close { <.ws> <close_token> }
+token open_list { <.ws> <open_list_token> }
+token close_list { <.ws> <close_list_token> }
+token open_curly { <.ws> <open_curly_token> }
+token close_curly { <.ws> <close_curly_token> }
+token ht_sep { <.ws> <head_tail_separator_token> }
+token comma { <.ws> <comma_token> }
+token end { <.ws> <end_token> }
 
-# Layout text, section 6.4.1.
-token ws { <layout_text>+ } # Layout text separates stuff, so we set <ws> to that
-token layout_text { [ <layout_char> | <comment> ]+ }
+# Layout text, section 6.4.1
+token ws { <.layout_text>+ } # Layout text separates stuff, so we set <ws> to that
+token layout_text { [ <.layout_char> | <.comment>  ]+ }
 proto token comment { <...> }
-token comment:sym<line> { <line_comment> \N \n }
+token comment:sym<line> { <.line_comment> \N \n  }
 token comment:sym<bracketed> {
     '/*'
     [ <-[/]> | <!after '*' > '/' ] *
@@ -153,6 +193,8 @@ proto token name_token { <...> }
 token name_token:sym<identifier> { <small_char> <alnum_char>* }
 token name_token:sym<graphic> { <+graphic_char +backslash>+ }
 token name_token:sym<quoted> { <?[']> <quote_EXPR> } # For vim: ' ]> }
+token name_token:sym<semicolon> { <semicolon_token> }
+token name_token:sym<cut> { <cut_token> }
 
 # We might be able to get some of this for free using quote_EXPR's quotemod
 # features, but I'm not quite sure if it'll work exactly as I want to.
@@ -195,11 +237,30 @@ token hex_constant { 0x <hexdigit_char>+ }
 token float_token { <integer_constant> '.' <decdigit_char>+ <exponent>? }
 token exponent { <[eE]> <[\-+]>? <integer_constant> }
 
+# Character code lists: section 6.4.6
+# TODO
+
+# Back quoted strings: section 6.4.7
+# TODO
+
+# Other tokens: section 6.4.8
+token open_token { <open_char> }
+token close_token { <close_char> }
+token open_list_token { <open_list_char> }
+token close_list_token { <close_list_char> }
+token open_curly_token { <open_curly_char> }
+token close_curly_token { <close_curly_char> }
+token head_tail_separator_token { <head_tail_separator_char> }
+token comma_token { <comma_char> }
+token end_token { <end_char> }
+
+token end_char { '.' }
+
 # Characters: section 6.5
 token char { <+graphic_char +alnum_char +solo_char +layout_char +meta_char> }
 
 # Graphic characters: section 6.5.1
-token graphic_char { <[#$&*+_./:<=>?@^~]> }
+token graphic_char { <[#$&*+\-./:<=>?@^~]> }
 
 # Alphanumerics: section 6.5.2
 token alnum_char { <+alpha_char +digit> }
@@ -225,16 +286,16 @@ token solo_char { <+cut_char
                    +close_curly_char
                    +head_tail_char
                    +line_comment_char> }
-token cut_char { <[!]> }
-token open_char { <[(]> }
-token close_char { <[)]> }
-token comma_char { <[,]> }
-token semicolon_char { <[;]> }
-token open_list_char { <[\[]> }
-token close_list_char { <[\]]> }
-token open_curly_char { <[{]> }
-token close_curly_char { <[}]> }
-token head_tail_char { <[|]> }
+token cut_char          { <[!]> }
+token open_char         { <[(]> }
+token close_char        { <[)]> }
+token comma_char        { <[,]> }
+token semicolon_char    { <[;]> }
+token open_list_char    { <[\[]> }
+token close_list_char   { <[\]]> }
+token open_curly_char   { <[{]> }
+token close_curly_char  { <[}]> }
+token head_tail_char    { <[|]> }
 token line_comment_char { <[%]> }
 
 # Layout characters: section 6.5.4
@@ -249,30 +310,3 @@ token backslash_char { <[\\]> }
 token single_quote_char { <[']> }
 token double_quote_char { <["]> }
 token back_quote_char { <[`]> }
-
-## Lexer items
-
-## Statements
-
-rule statementlist { [ <statement> | <?> ] ** ';' }
-
-rule statement {
-    | <statement_control>
-    | <EXPR>
-}
-
-proto token statement_control { <...> }
-rule statement_control:sym<say>   { <sym> [ <EXPR> ] ** ','  }
-rule statement_control:sym<print> { <sym> [ <EXPR> ] ** ','  }
-
-## Terms
-
-token term:sym<integer> { <integer> }
-token term:sym<quote> { <quote> }
-
-proto token quote { <...> }
-token quote:sym<'> { <?[']> <quote_EXPR: ':q'> } # For vi: ' ]> }
-token quote:sym<"> { <?["]> <quote_EXPR: ':qq'> } # For vi: " ]> }
-
-# TODO: Operators. ISO Prolog allows for user defined operators. That should
-# probably be implemented using nqp's extendable grammar features.
