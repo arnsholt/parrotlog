6ffc84e477b88123072e8f46bba9d8aa2d12bef7
Arne <arnsholt@gmail.com>
2010-04-22 00:49:24 +0200

    Getting closer to actually generating PAST.

--- src/parrotlog/PAST.pm
+++ src/parrotlog/PAST.pm
@@ -1,33 +1,67 @@
-sub make_past($ast) {
-    if $ast ~~ Variable {
-        pir::say("Can't make AST for a Variable!");
-    }
-    elsif $ast ~~ Term {
 =begin workflow
 
-This'll have to be dispatched on the basis of the top-level functor of the
-term. For now, we're parsing queries (not actual programs) so I think the way
-to do it is to work recursively. If the functor is =, set up the environment
-with the necessary parameters, and then call unify. If it's , do make_ast() on
-the lhs first, then on the rhs.
+First, we set up the outermost context of the query: we create named variables
+for all of the named variables used in the query.
+
+Then we recursively traverse the Term representing the query. It might make
+sense to create custom PAST types for unification and certain other ops
+(subroutine application, certainly) in order to make this code a bit more
+legible, seeing how all of these will require additional function calls to set
+up the backtracking infrastructure.
 
 =end workflow
-        if $ast.functor eq '=' {
-            # Set up call to unify().
-            pir::say("unification");
+
+=begin selfnotes
+
+- Function calls seem to be created with PAST::Op and pasttype `call'.
+- New objects with pasttype `new'?
+
+=end selfnotes
+
+sub make_past($ast) {
+    # TODO: First we set up the outer lexical scope by creating all the named
+    # variables in the query. Finally we set the mark to stop the pruning in
+    # case of a cut.
+    my $block := PAST::Block.new(:blocktype<immediate>);
+    # For variables in $ast: $block.push(make_variable($varname))
+
+    # Then we process the query as typed in.
+    make_conjunction($ast);
+}
+
+sub make_unification($ast) {
+    # TODO: Set up call to unify().
+    pir::say("unification");
+}
+
+sub make_conjunction($ast) {
+    if $ast ~~ Term {
+        if $ast.functor eq '=' && $ast.arity == 2 {
+            make_unification($ast);
         }
-        elsif $ast.functor eq ',' {
-            # Create ast for lhs first, then for rhs (with lhs as outer
-            # scope).
+        elsif $ast.functor eq ',' && $ast.arity == 2 {
             pir::say("conjunction");
-            my $lhs := make_past($ast.args[0]);
-            my $rhs := make_past($ast.args[1]);
+            my $lhs := make_conjunction($ast.args[0]);
+            my $rhs := make_conjunction($ast.args[1]);
+        }
+        elsif $ast.functor eq '!' && $ast.arity == 0 {
+            make_cut();
         }
         else {
-            pir::die("Don't know how to handle functor {$ast.functor}");
+            pir::die("Don't know how to handle {$ast.functor}/{$ast.arity}");
         }
     }
     else {
-        pir::die("Non-Term, non-Variable passed to make_ast()");
+        pir::die("Non-Term passed to make_ast()");
     }
+}
+
+sub make_cut() {
+    pir::say("cut");
+}
+
+sub make_term($ast) {
+}
+
+sub make_variable($ast) {
 }
